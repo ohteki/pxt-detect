aa060186a21a42c01b38d996b6b8af59ee8a01e6
yusukeOteki <ut37186443@gmail.com>
2018-08-02 09:05:47 +0900

    test

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% weight=70 icon="\uf1ec" color=#00bbd7
+//% weight=70 icon="\uf1ec" color=#00bbd7 block="Detector"
 namespace detector {
     //% blockId=show_number block="Show a number %num"
     export function showNumber(num: number = 0): void {
