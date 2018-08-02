c38d2d25839b7bbe0cdbc23d5ced23be2edf3ade
yusukeOteki <ut37186443@gmail.com>
2018-08-02 09:45:05 +0900

    test

--- main.ts
+++ main.ts
@@ -1,20 +1,19 @@
 //% weight=70 icon="\uf1ec" color=#00bbd7 block="Detector"
 namespace detector {
     //% blockId=show_number block="Show a number %num"
+    let initialValue = 0
+
     export function showNumber(num: number = 0): void {
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . . . . .
-        # . . . #
-        . # # # .
-        `);
+      initialValue = num
     }
 
     //% blockId=sbrick_measured_value
-    //% block="measured value" shim=sbrick::measuredValue
-    export function measuredValue(): number
-    {
+    //% block="measured value %num from %f"
+    export function measuredValue(num: number = 0, f: number = 10): number{
+      if(num < initialValue*(100-f)/100 || initialValue*(100+f)/100 < num){
+        return 1
+      }else{
         return 0
+      }
     }
 }
