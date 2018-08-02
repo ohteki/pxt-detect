2f9430d53719dc37f6bcc28f8dced977a1deff92
yusukeOteki <ut37186443@gmail.com>
2018-08-02 10:18:35 +0900

    test

--- main.ts
+++ main.ts
@@ -2,14 +2,24 @@
 namespace detector {
     let initialValue = 0
 
-    //% blockId=show_number block="the initial value %num"
-    export function showNumber(num: number = 0): number {
+    //% blockId=set_initial_value block="the initial value %num"
+    export function setInitialValue(num: number = 0): number {
       return num
     }
 
-    //% blockId=sbrick_measured_value
+    //% blockId=Evaluating_measured_value
     //% block="%num|changed %f| from %init"
-    export function measuredValue(num: number = 0, f: number = 10, init: number = initialValue): number{
+    export function Evaluating(num: number = 0, f: number = 10, init: number = initialValue): number{
+      if(num < init*(100-f)/100 || init*(100+f)/100 < num){
+        return 1
+      }else{
+        return 0
+      }
+    }
+
+    //% blockId=Evaluating_measured_value_boolean
+    //% block="%num|changed %f| from %init"
+    export function measuredValue(num: number = 0, f: number = 10, init: number = initialValue): boolean{
       if(num < init*(100-f)/100 || init*(100+f)/100 < num){
         return 1
       }else{
