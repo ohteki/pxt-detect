bb33eb76825b5494c2c92c4237ac8460f2275be1
yusukeOteki <ut37186443@gmail.com>
2018-08-02 10:21:34 +0900

    test

--- main.ts
+++ main.ts
@@ -15,4 +15,13 @@ namespace detector {
       }
     }
 
+    //% blockId=Evaluating_measured_value_boolean
+    //% block="%num|changed %f| from %init"
+    export function measuredValue(num: number = 0, f: number = 10, init: number = initialValue): boolean{
+      if(num < init*(100-f)/100 || init*(100+f)/100 < num){
+        return true
+      }else{
+        return false
+      }
+    }
 }
