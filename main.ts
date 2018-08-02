ceab7dec14793147cd18745ce5d4dc51864e1a19
yusukeOteki <ut37186443@gmail.com>
2018-08-02 09:47:07 +0900

    test

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 //% weight=70 icon="\uf1ec" color=#00bbd7 block="Detector"
 namespace detector {
-    //% blockId=show_number block="Show a number %num"
     let initialValue = 0
 
+    //% blockId=show_number block="Show a number %num"
     export function showNumber(num: number = 0): void {
       initialValue = num
     }
 
     //% blockId=sbrick_measured_value
-    //% block="measured value %num from %f"
-    export function measuredValue(num: number = 0, f: number = 10): number{
+    //% block="%num changed %f from %init"
+    export function measuredValue(num: number = 0, f: number = 10, init: number = initialValue): number{
       if(num < initialValue*(100-f)/100 || initialValue*(100+f)/100 < num){
         return 1
       }else{
