831c244e4ced58b8793d78d1fbc534c84ef8bf47
yusukeOteki <ut37186443@gmail.com>
2018-08-02 10:20:09 +0900

    test

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
 //% weight=70 icon="\uf1ec" color=#00bbd7 block="Detector"
 namespace detector {
-    let initialValue = 0
-
     //% blockId=set_initial_value block="the initial value %num"
     export function setInitialValue(num: number = 0): number {
       return num
@@ -9,7 +7,7 @@ namespace detector {
 
     //% blockId=Evaluating_measured_value
     //% block="%num|changed %f| from %init"
-    export function Evaluating(num: number = 0, f: number = 10, init: number = initialValue): number{
+    export function Evaluating(num: number, f: number, init: number): number{
       if(num < init*(100-f)/100 || init*(100+f)/100 < num){
         return 1
       }else{
@@ -17,13 +15,4 @@ namespace detector {
       }
     }
 
-    //% blockId=Evaluating_measured_value_boolean
-    //% block="%num|changed %f| from %init"
-    export function measuredValue(num: number = 0, f: number = 10, init: number = initialValue): boolean{
-      if(num < init*(100-f)/100 || init*(100+f)/100 < num){
-        return 1
-      }else{
-        return 0
-      }
-    }
 }
