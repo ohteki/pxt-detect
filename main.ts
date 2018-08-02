df916698e0df7fd42a856ead2b6c26650f8bc61c
yusukeOteki <ut37186443@gmail.com>
2018-08-02 09:47:42 +0900

    test

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ namespace detector {
     //% blockId=sbrick_measured_value
     //% block="%num changed %f from %init"
     export function measuredValue(num: number = 0, f: number = 10, init: number = initialValue): number{
-      if(num < initialValue*(100-f)/100 || initialValue*(100+f)/100 < num){
+      if(num < init*(100-f)/100 || init*(100+f)/100 < num){
         return 1
       }else{
         return 0
