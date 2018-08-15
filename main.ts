87094746fba9a510e97d8c583a727ce806d3da61
yusukeOteki <ut37186443@gmail.com>
2018-08-15 23:49:18 +0900

    initial values were removed & serial blocks were added

--- main.ts
+++ main.ts
@@ -1,16 +1,18 @@
 //% weight=70 icon="\uf1ec" color=#4ab500 block="detect"
 namespace detect {
-    //% blockId=set_initial_value block="the initial value %num|of -"
-    export function setInitialValue(num: number = 0): number {
-      return num
-    }
+  //% blockId=set_initial_value block="the initial value %num|of -"
+  export function setInitialValue(num: number = 0): number {
+    serial.writeNumber(num)
+    return num
+  }
 
-    //% blockId=Evaluating_measured_value block="%num|changed %f| from %init|of -"
-    export function measuredValue(num: number = 0, f: number = 10, init: number = initialValue): boolean{
-      if(num < init*(100-f)/100 || init*(100+f)/100 < num){
-        return true
-      }else{
-        return false
-      }
+  //% blockId=Evaluating_measured_value block="%num|changed %f| from %init|of -"
+  export function measuredValue(num: number, f: number, init: number): boolean {
+    serial.writeLine("measuredValue=" + num.toString() + ", changedValueFromInitial=" + (100 * (num - init) / init).toString() + "%, returnValue=" + ((num < init * (100 - f) / 100 || init * (100 + f) / 100 < num) ? "True" : "False"))
+    if (num < init * (100 - f) / 100 || init * (100 + f) / 100 < num) {
+      return true
+    } else {
+      return false
     }
+  }
 }
